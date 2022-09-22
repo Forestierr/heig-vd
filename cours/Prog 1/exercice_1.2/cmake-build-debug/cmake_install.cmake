9783e1f417f6ad55fd424a84b1f7784eb8b28885
Robin Forestier <robinforestier1407@gmail.com>
2022-09-22 09:11:14 +0200

    add git ignore

--- cours/Prog 1/exercice_1.2/cmake-build-debug/cmake_install.cmake
+++ cours/Prog 1/exercice_1.2/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/robin/Documents/HEIG/heig-vd-git/cours/Prog 1/exercice_1_2
+# Install script for directory: C:/Users/robin/Documents/HEIG/heig-vd-git/cours/Prog 1/exercice_1.2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/robin/Documents/HEIG/heig-vd-git/cours/Prog 1/exercice_1_2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/robin/Documents/HEIG/heig-vd-git/cours/Prog 1/exercice_1.2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
