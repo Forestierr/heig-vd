242792e98ecfd1a6295ddf2fb0cda0ff92bbe27c
Robin Forestier <robinforestier1407@gmail.com>
2022-10-14 10:17:09 +0200

    test color

--- cours/Prog 1/optimisation/main.cpp
+++ cours/Prog 1/optimisation/main.cpp
@@ -2,15 +2,58 @@
 #include <windows.h>
 
 using namespace std;
+/*
+The different color codes are
+
+https://dev.to/tenry/terminal-colors-in-c-c-3dgc
+https://cplusplus.com/forum/beginner/54360/
+
+0   BLACK
+1   BLUE
+2   GREEN
+3   CYAN
+4   RED
+5   MAGENTA
+6   BROWN
+7   LIGHTGRAY
+8   DARKGRAY
+9   LIGHTBLUE
+10  LIGHTGREEN
+11  LIGHTCYAN
+12  LIGHTRED
+13  LIGHTMAGENTA
+14  YELLOW
+15  WHITE
+*/
 
 int main(void) {
 
-    system("color 2");
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
-    system("echo test");
-    system("color 3");
+    SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
+    printf("red text\n");
 
-    system("echo test");
+    SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | BACKGROUND_BLUE);
+    printf("yellow on blue\n");
+
+
+    for (int i = 0; i <= 15; i++) {
+        SetConsoleTextAttribute(hConsole, i);
+        cout << "test" << endl;
+    }
+
+
+    for (int j = 0; j <= 255; j += 16) {
+        SetConsoleTextAttribute(hConsole, j);
+        cout << " ";
+    }
+
+    cout << endl;
+
+    for (int j = 0; j <= 255; j ++) {
+        SetConsoleTextAttribute(hConsole, j);
+        cout << "X";
+    }
 
     return 0;
 }
