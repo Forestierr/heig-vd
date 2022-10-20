8d92819c8a59cf2990805baba1accfaff50aca4c
Robin Forestier <robinforestier1407@gmail.com>
2022-10-20 08:51:44 +0200

    test speed

--- cours/Prog 1/optimisation/main.cpp
+++ cours/Prog 1/optimisation/main.cpp
@@ -1,58 +1,11 @@
 #include <iostream>
-#include <windows.h>
 
 using namespace std;
-/*
-The different color codes are
-
-https://dev.to/tenry/terminal-colors-in-c-c-3dgc
-https://cplusplus.com/forum/beginner/54360/
-
-0   BLACK
-1   BLUE
-2   GREEN
-3   CYAN
-4   RED
-5   MAGENTA
-6   BROWN
-7   LIGHTGRAY
-8   DARKGRAY
-9   LIGHTBLUE
-10  LIGHTGREEN
-11  LIGHTCYAN
-12  LIGHTRED
-13  LIGHTMAGENTA
-14  YELLOW
-15  WHITE
-*/
 
 int main(void) {
 
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-
-    SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
-    printf("red text\n");
-
-    SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | BACKGROUND_BLUE);
-    printf("yellow on blue\n");
-
-
-    for (int i = 0; i <= 15; i++) {
-        SetConsoleTextAttribute(hConsole, i);
-        cout << "test" << endl;
-    }
-
-
-    for (int j = 0; j <= 255; j += 16) {
-        SetConsoleTextAttribute(hConsole, j);
-        cout << " ";
-    }
-
-    cout << endl;
-
-    for (int j = 0; j <= 255; j ++) {
-        SetConsoleTextAttribute(hConsole, j);
-        cout << "X";
+    for (int i = 0; i < 100; i++) {
+        cout << "i" << endl;
     }
 
     return 0;
